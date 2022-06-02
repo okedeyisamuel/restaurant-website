c4d8809e8afad7a21ca3666d77d80cd024011063
iamtoluwalope <okedeyisamuel@gmail.com>
2022-06-02 21:30:39 +0100

    added website files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./public/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#E9DAC1',
+        'secondary': '#A97155',
+        'tertiary': '#632626'
+        
+      },
+      fontFamily :{
+        'body': ['Nunito'] 
+      },
+      backgroundImage: {
+                'food': "url('/public/img/suya.jpg')"
+            },
+    },
+  },
+  plugins: [],
+}
